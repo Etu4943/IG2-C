2811ebf3c58ad99888065f291e74120c8fdc4cef
Etu4943 <etu49438@henallux.be>
2022-09-25 18:49:25 +0200

    Insertion des exos js du premier labo / intro

---
+++ Q1/Techno_Web/JavaScript/Ex3/boutton.js
@@ -0,0 +1,11 @@
+let nbClicks = 0;
+
+function estClique_boutton(){
+    nbClicks += 1;
+    alert("Nombre de cliques : " + nbClicks);
+}
+
+function estClique_lien(){
+    nbClicks = 0;
+    alert("Compteur remis à 0 !");
+}
---
+++ Q1/Techno_Web/JavaScript/Ex3/multiplication.js
@@ -0,0 +1,18 @@
+
+let nombreObtenu = Number(prompt("Donnez un nombre : "));
+
+while(!isFinite(nombreObtenu)){
+    nombreObtenu = Number(prompt("Donnez un nombre : "));
+}
+
+function affichageNombreObtenu(){
+    document.write("<p class='nombre'>" + nombreObtenu + "</p>");
+}
+function tableMutliplication(){
+    for(let iAge = 1 ; iAge <=10 ; iAge ++){
+        document.write("<tr><td>"+ nombreObtenu + "</td> <td>" + iAge + "</td> <td>" + nombreObtenu * iAge + "</td></tr>");
+    }
+}
+
+
+
