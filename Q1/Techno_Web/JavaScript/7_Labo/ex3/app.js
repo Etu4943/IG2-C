d423a42c65060e754b67c25ba79658213f891a50
Etu4943 <etu49438@henallux.be>
2022-10-25 17:54:17 +0200

    Avancement JS7

--- Q1/Techno_Web/JavaScript/7_Labo/ex3/app.js
+++ Q1/Techno_Web/JavaScript/7_Labo/ex3/app.js
@@ -5,10 +5,17 @@ window.onload = init;
 
 function init(){
     let currentValue;
-    
+    majValeur(2);
 
 }
 
-function majVeleur(newValue){
+function majValeur(newValue){
     document.getElementById("spanVal").innerText = newValue;
+}
+
+function calcOpposé(){
+    let value = parseInt(document.getElementById("spanVal").innerText);
+    let resultat = value *-1;
+    console.log(resultat);
+    majValeur(resultat);
 }
