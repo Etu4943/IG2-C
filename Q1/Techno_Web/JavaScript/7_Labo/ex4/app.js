f704e8ebf0e55d115417fc70442bdddbfea50de6
Etu4943 <etu49438@henallux.be>
2022-10-25 22:28:08 +0200

    Fin ex4 JS7

--- Q1/Techno_Web/JavaScript/7_Labo/ex4/app.js
+++ Q1/Techno_Web/JavaScript/7_Labo/ex4/app.js
@@ -10,16 +10,24 @@ function clic(numBloc){
     if(numBloc != grandActuel){
         let width_futur_grand = 30;
         let width_futur_petit = 490;
-        while(width_futur_grand <= 500){
-            width_futur_grand = setTimeout(modWidth, 1000, grandActuel,width_futur_petit);
-            width_futur_petit = setTimeout(modWidth, 1000, numBloc,width_futur_grand);
-        }
-        grandActuel = numBloc;
+        loop(width_futur_grand,width_futur_petit,numBloc);
     }
+}
+
+function loop(width_futur_grand,width_futur_petit,numBloc){
+    setTimeout(() => {
+        if(width_futur_grand <= 500){
+            modWidth(grandActuel,width_futur_petit);
+            modWidth(numBloc,width_futur_grand);
+            width_futur_grand += 10;
+            width_futur_petit -= 10;
+            loop(width_futur_grand,width_futur_petit,numBloc);
+        } else
+            grandActuel = numBloc;
+    },100);
 }
 
 function modWidth(numBloc,size){
     let bloc = document.querySelector("#bloc"+numBloc);
     bloc.style.width = `${size}px`;
-    return size + (numBloc == grandActuel ? -10 : 10);
 }
